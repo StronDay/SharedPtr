dfd6909fcf9c4c8d025285004e0da8041cfc7da9
StronDay <StronDay@outlook.com>
2022-05-12 15:49:03 +0300

    Initial project version

---
+++ SharedPtr.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class SharedPtr
+{
+public: 
+
+    
+
+private:
+
+    Type* _pointer = nullptr;
+    size_t* counter = nullptr;
+};
